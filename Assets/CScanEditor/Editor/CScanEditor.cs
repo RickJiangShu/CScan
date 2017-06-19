824e3db34bf68e9a175d7a9a23de798189c74676
Rick <rickjiangshu@gmail.com>
2017-06-19 14:26:04 +0800

    Rename

---
+++ Assets/CScanEditor/Editor/CScanEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+
+public class CScanEditor : Editor{
+
+    /// <summary>
+    /// 打印所有
+    /// </summary>
+    [MenuItem("CScan/All")]
+    static void PrintAll()
+    {
+        object[] files = GetSelectedCodeFiles();
+    }
+
+    /// <summary>
+    /// 获取选中的代码文件
+    /// </summary>
+    /// <returns></returns>
+    static object[] GetSelectedCodeFiles()
+    {
+        object[] files = Selection.GetFiltered(typeof(FileInfo), SelectionMode.DeepAssets);
+
+        var obj = Selection.activeObject;
+
+        UnityEngine.Debug.Log("activeObject:" + Selection.activeObject);
+        return null;
+    }
+}
