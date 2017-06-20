13bd0bf9f61d8c1a9406bb610e7aeb382371fe55
Rick <rickjiangshu@gmail.com>
2017-06-20 17:39:38 +0800

    number thousand

--- Editor/CScanEditor.cs
+++ Editor/CScanEditor.cs
@@ -60,7 +60,7 @@ public class CScanEditor : Editor{
         float scanSeconds = (Timestamp() - startTime) * 0.001f;
 
         string scanName = !ignore ? "所有行" : "有效行";
-        UnityEngine.Debug.Log(string.Format("共扫描{0}个文件，{1}总共：{2}行\n选取文件耗时：{3}s，扫描耗时：{4}s", count, scanName, lineCount, selectSeconds.ToString("0.000"), scanSeconds.ToString("0.000")));
+        UnityEngine.Debug.Log(string.Format("共扫描{0}个文件，{1}总共：{2}行\n选取文件耗时：{3}s，扫描耗时：{4}s", count.ToString("N0"), scanName, lineCount.ToString("N0"), selectSeconds.ToString("0.000"), scanSeconds.ToString("0.000")));
     }
 
     /// <summary>
