95ef5bec1b3cac3ac32041998f19e3d7b4d3a197
Rick <rickjiangshu@gmail.com>
2017-06-20 17:54:54 +0800

    Tips

--- Editor/CScanEditor.cs
+++ Editor/CScanEditor.cs
@@ -60,7 +60,7 @@ public class CScanEditor : Editor{
         float scanSeconds = (Timestamp() - startTime) * 0.001f;
 
         string scanName = !ignore ? "所有行" : "有效行";
-        UnityEngine.Debug.Log(string.Format("共扫描{0}个文件，{1}总共：{2}行\n选取文件耗时：{3}s，扫描耗时：{4}s", count.ToString("N0"), scanName, lineCount.ToString("N0"), selectSeconds.ToString("0.000"), scanSeconds.ToString("0.000")));
+        UnityEngine.Debug.Log(string.Format("本次共扫描{0}个文件，{1}总共：{2}行\n选取文件耗时：{3}s，扫描耗时：{4}s", count.ToString("N0"), scanName, lineCount.ToString("N0"), selectSeconds.ToString("0.000"), scanSeconds.ToString("0.000")));
     }
 
     /// <summary>
@@ -70,6 +70,7 @@ public class CScanEditor : Editor{
     /// <param name="lineCount"></param>
     public static void ScanLines(string content,out int lineCount)
     {
+        
         lineCount = 1;
         for (int i = 0, l = content.Length; i < l; i++)
         {
@@ -78,8 +79,10 @@ public class CScanEditor : Editor{
                 lineCount++;
             }
         }
+         
+        
         /*
-        MatchCollection collection = Regex.Matches(content, LF, RegexOptions.Multiline);
+        MatchCollection collection = Regex.Matches(content, LF);
         lineCount = collection.Count + 1;
          */
     }
