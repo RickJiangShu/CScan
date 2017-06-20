32f04a91f0b40d3216649ab2cb1b57c5d15a7a07
Rick <rickjiangshu@gmail.com>
2017-06-20 17:35:52 +0800

    add progress

--- Editor/CScanEditor.cs
+++ Editor/CScanEditor.cs
@@ -16,18 +16,7 @@ public class CScanEditor : Editor{
     [MenuItem("CScan/All")]
     static void PrintAll()
     {
-        object[] files = GetSelectedCodeFiles();
-        int count = files.Length;
-        int lineCount = 0;
-        for (int i = 0; i < count; i++)
-        {
-            string fileContent = files[i].ToString();
-            int fileLineCount;
-            ScanLines(fileContent, out fileLineCount);
-            lineCount += fileLineCount;
-        }
-
-        UnityEngine.Debug.Log("共扫描" + count + "个文件，所有行总共：" + lineCount + "行");
+        Scan(false);
     }
 
     /// <summary>
@@ -36,18 +25,42 @@ public class CScanEditor : Editor{
     [MenuItem("CScan/Ignore Invalid")]
     static void IgnoreInvalid()
     {
+        Scan(true);
+    }
+
+
+    static void Scan(bool ignore)
+    {
+        //选取
+        EditorUtility.DisplayProgressBar("CScan", "正在选取脚本文件...", 0f);
+        long startTime = Timestamp();
         object[] files = GetSelectedCodeFiles();
+        float selectSeconds = (Timestamp() - startTime) * 0.001f;
+
+        //扫描
+        startTime = Timestamp();
         int count = files.Length;
         int lineCount = 0;
         for (int i = 0; i < count; i++)
         {
-            string fileContent = files[i].ToString();
+            MonoScript src = files[i] as MonoScript;
+            string fileContent = src.text;
             int fileLineCount;
-            ScanValidLines(fileContent, out fileLineCount);
-            lineCount += fileLineCount;
-        }
+            if (!ignore)
+                ScanLines(fileContent, out fileLineCount);
+            else
+                ScanValidLines(fileContent, out fileLineCount);
 
-        UnityEngine.Debug.Log("共扫描" + count + "个文件，有效行总共：" + lineCount + "行");
+            lineCount += fileLineCount;
+            EditorUtility.DisplayProgressBar("CScan", string.Format("正在扫描{0}...{1}/{2}", src.name, i, count), (float)i / count);
+
+        }
+        EditorUtility.ClearProgressBar();
+
+        float scanSeconds = (Timestamp() - startTime) * 0.001f;
+
+        string scanName = !ignore ? "所有行" : "有效行";
+        UnityEngine.Debug.Log(string.Format("共扫描{0}个文件，{1}总共：{2}行\n选取文件耗时：{3}s，扫描耗时：{4}s", count, scanName, lineCount, selectSeconds.ToString("0.000"), scanSeconds.ToString("0.000")));
     }
 
     /// <summary>
@@ -57,8 +70,18 @@ public class CScanEditor : Editor{
     /// <param name="lineCount"></param>
     public static void ScanLines(string content,out int lineCount)
     {
+        lineCount = 1;
+        for (int i = 0, l = content.Length; i < l; i++)
+        {
+            if (content[i] == '\n')
+            {
+                lineCount++;
+            }
+        }
+        /*
         MatchCollection collection = Regex.Matches(content, LF, RegexOptions.Multiline);
         lineCount = collection.Count + 1;
+         */
     }
     /// <summary>
     /// 扫描有效行
@@ -85,5 +108,14 @@ public class CScanEditor : Editor{
     {
         var scripts = Selection.GetFiltered(typeof(MonoScript), SelectionMode.DeepAssets);//C#、JS、Boo
         return scripts;
+    }
+
+    /// <summary>
+    /// 获取1970.01.01到现在的时间戳（毫秒）
+    /// </summary>
+    /// <returns></returns>
+    static long Timestamp()
+    {
+        return (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000;
     }
 }
