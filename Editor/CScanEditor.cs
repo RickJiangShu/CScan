181f14801e9cffa36ab3ad8d5a5f769ae5e7408a
Rick <rickjiangshu@gmail.com>
2017-06-21 11:18:55 +0800

    tips

--- Editor/CScanEditor.cs
+++ Editor/CScanEditor.cs
@@ -6,8 +6,8 @@ public class CScanEditor : Editor{
 
     private static string LF = @"\r?\n";//仅匹配换行
 
-    private static string LineComment = @"//.*" + LF;//行注释
-    private static string BlockComment = @"/\*[\s\S]*\*/";//块注释
+    private static string LineComment = @"\/\/[^\n]*";//行注释
+    private static string BlockComment = @"\/\*(\s|.)*?\*\/";//块注释
     private static string NullLines = LF + @"\s*" + LF;//空白行
 
     /// <summary>
@@ -32,15 +32,24 @@ public class CScanEditor : Editor{
     static void Scan(bool ignore)
     {
         //选取
-        EditorUtility.DisplayProgressBar("CScan", "正在选取脚本文件...", 0f);
+        EditorUtility.DisplayProgressBar("CScan", "正在选取代码文件...", 0f);
         long startTime = Timestamp();
         object[] files = GetSelectedCodeFiles();
         float selectSeconds = (Timestamp() - startTime) * 0.001f;
+
+        if (files.Length == 0)
+        {
+            EditorUtility.ClearProgressBar();
+            UnityEngine.Debug.LogError("找不到代码文件！请在Project窗口选中需要扫描的文件或文件夹！");
+            return;
+        }
 
         //扫描
         startTime = Timestamp();
         int count = files.Length;
         int lineCount = 0;
+        int maxLineCount = 0;//最多行
+        string maxFileName = "";//最多行的文件名
         for (int i = 0; i < count; i++)
         {
             MonoScript src = files[i] as MonoScript;
@@ -51,6 +60,12 @@ public class CScanEditor : Editor{
             else
                 ScanValidLines(fileContent, out fileLineCount);
 
+            if (fileLineCount > maxLineCount)
+            {
+                maxLineCount = fileLineCount;
+                maxFileName = src.name;
+            }
+
             lineCount += fileLineCount;
             EditorUtility.DisplayProgressBar("CScan", string.Format("正在扫描{0}...{1}/{2}", src.name, i, count), (float)i / count);
 
@@ -60,7 +75,7 @@ public class CScanEditor : Editor{
         float scanSeconds = (Timestamp() - startTime) * 0.001f;
 
         string scanName = !ignore ? "所有行" : "有效行";
-        UnityEngine.Debug.Log(string.Format("本次共扫描{0}个文件，{1}总共：{2}行\n选取文件耗时：{3}s，扫描耗时：{4}s", count.ToString("N0"), scanName, lineCount.ToString("N0"), selectSeconds.ToString("0.000"), scanSeconds.ToString("0.000")));
+        UnityEngine.Debug.Log(string.Format("本次共扫描{0}个文件，{1}总共：{2}行（行数最多的是：{3},{4}行）\n选取文件耗时：{5}s，扫描耗时：{6}s", count.ToString("N0"), scanName, lineCount.ToString("N0"), maxFileName, maxLineCount.ToString("N0"), selectSeconds.ToString("0.000"), scanSeconds.ToString("0.000")));
     }
 
     /// <summary>
@@ -94,7 +109,7 @@ public class CScanEditor : Editor{
         string validContent = content;
 
         validContent = validContent.Trim();
-        validContent = Regex.Replace(validContent, LineComment, "\n");
+        validContent = Regex.Replace(validContent, LineComment, "");
         validContent = Regex.Replace(validContent, BlockComment, "");
         validContent = Regex.Replace(validContent, NullLines, "\n");
 
